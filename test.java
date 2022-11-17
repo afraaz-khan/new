4187398fadd0dfa3e46f1416eafc17369580ab6c
afraaz-khan <118386746+afraaz-khan@users.noreply.github.com>
2022-11-17 10:26:06 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class test 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter a value= ");
+        Scanner scan = new Scanner(System.in);
+        int age = scan.nextInt(); 
+        System.out.println(age);
+    }
+}
